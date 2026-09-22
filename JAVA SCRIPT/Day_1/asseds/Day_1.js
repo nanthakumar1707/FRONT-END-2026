08b99445a29276a2becdfb4afb9521aefeda11fb
nanthakumar <nanthakumar001nk@gmail.com>
2026-09-22 22:39:36 +0530

    add file

---
+++ JAVA SCRIPT/Day_1/asseds/Day_1.js
@@ -0,0 +1,108 @@
+// ==========================================
+// TASK 1 - 30 NAME - ONE CASE
+// ==========================================
+
+let studentName = "Nantha";
+let studentAge = 23;
+let studentEmail = "nantha001nk@gmail.com";
+let studentPhone = "9876543210";
+let studentCity = "Chennai";
+let collegeName = "ABC College";
+let courseName = "Full Stack Development";
+let courseDuration = "6 Months";
+let teacherName = "";
+let subjectName = "JavaScript";
+let classRoom = "C101";
+let studentId = 101;
+let rollNumber = 25;
+let departmentName = "Computer Science";
+let collegeCode = "ABC123";
+let totalMarks = 450;
+let obtainedMarks = 400;
+let attendancePercentage = 90;
+let isStudent = true;
+let isPresent = true;
+let favouriteColor = "Blue";
+let favouriteFood = "Pizza";
+let favouriteGame = "Cricket";
+let favouritePlace = "Chennai";
+let fatherName = "";
+let motherName = "";
+let brotherName = "";
+let sisterName = "";
+let joiningYear = 2026;
+let graduationYear = 2028;
+
+
+// ==========================================
+// TASK 2 - VARIABLE RETURN / CONSOLE PRINT
+// 5 PRINTS
+// ==========================================
+
+let name = "Nantha";
+let age = 23;
+let course = "Full Stack Development";
+let city = "Thoothukudi";
+let studentStatus = true;
+
+console.log(name);
+console.log(age);
+console.log(course);
+console.log(city);
+console.log(studentStatus);
+
+
+// ==========================================
+// TASK 3 - INITIALIZATION
+// 10 VALUES - VAR, LET, CONST
+// ==========================================
+
+var firstName = "Nantha";
+var userAge = 23;
+var userCity = "Chennai";
+
+let userCourse = "Full Stack Development";
+let mark = 95;
+let phoneNumber = "9876543210";
+
+const college = "ABC College";
+const joiningYearValue = 2026;
+const country = "India";
+const activeStudent = true;
+
+console.log(firstName);
+console.log(userAge);
+console.log(userCity);
+console.log(userCourse);
+console.log(mark);
+console.log(phoneNumber);
+console.log(college);
+console.log(joiningYearValue);
+console.log(country);
+console.log(activeStudent);
+
+
+// ==========================================
+// TASK 4 - DATA TYPES
+// ==========================================
+
+// String
+let dataName = "Nantha";
+
+// Number
+let dataAge = 23;
+
+// Boolean
+let dataStudent = true;
+
+// Null
+let dataAddress = null;
+
+// Undefined
+let dataPhone;
+
+console.log(dataName);
+console.log(dataAge);
+console.log(dataStudent);
+console.log(dataAddress);
+console.log(dataPhone);
